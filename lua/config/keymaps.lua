94c19eb8b98547a64ea87a6410755e24ec313ab7
dotwoffle <54816222+dotwoffle@users.noreply.github.com>
2026-07-24 12:19:49 -0600

    Snacks config was affecting other terminals, so just changed the ft keybinds instead

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,9 @@
 
 vim.keymap.set("n", "<A-h>", "<Cmd>tabp<cr>", { desc = "Previous Tab" })
 vim.keymap.set("n", "<A-h>", "<Cmd>tabn<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<leader>fT", function()
+  Snacks.terminal(nil, { win = { position = "right" } })
+end, { desc = "Terminal (cwd)" })
+vim.keymap.set("n", "<leader>ft", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root(), win = { position = "right" } })
+end, { desc = "Terminal (Root Dir)" })
